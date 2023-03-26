06965b93401e4dd39d318ca9fbb29f17d529df95
anjunact <anjunact@qq.com>
2023-03-27 00:36:17 +0800

    apps static

--- djmall/settings/develop.py
+++ djmall/settings/develop.py
@@ -1,4 +1,5 @@
 from .base import *
+
 ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
@@ -6,3 +7,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+STATICFILES_DIRS=[
+    BASE_DIR/f'themes{TEMPLATE_NAME}/static'
+]
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
